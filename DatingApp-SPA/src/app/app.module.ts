025174f6e07e7d1437771e431dea309b7ec1444f
unknown <kubajachowski@gmail.com>
2018-09-01 17:57:45 +0200

    end of section 4

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -4,16 +4,17 @@ import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
-import { HttpClient } from '../../node_modules/@types/selenium-webdriver/http';
 import { NavComponent } from './nav/nav.component';
 import { AuthService } from './_services/auth.service';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent,
-      NavComponent
+      NavComponent,
+      HomeComponent,
+      RegisterComponent
    ],
    imports: [
       BrowserModule,
@@ -21,7 +22,7 @@ import { AuthService } from './_services/auth.service';
       FormsModule
    ],
    providers: [
-       AuthService
+      AuthService
    ],
    bootstrap: [
       AppComponent
