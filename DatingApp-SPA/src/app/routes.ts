9c5384d9d805dcfbc222ee8b596b75b239b9fe48
unknown <kubajachowski@gmail.com>
2018-09-09 10:51:11 +0200

    end of section 10

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -7,6 +7,9 @@ import { MemberListComponent } from './members/member-list/member-list.component
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberDetailResolver } from './_resolver/member-details.resolver';
 import { MemberListResolver } from './_resolver/member-list.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { MemberEditResolver } from './_resolver/member-edit.resolver';
+import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 
 export const appRoutes: Routes = [
   {path: '', component: HomeComponent},
@@ -15,8 +18,13 @@ export const appRoutes: Routes = [
     runGuardsAndResolvers: 'always',
     canActivate: [AuthGuard],
     children: [
-      {path: 'members', component: MemberListComponent, resolve: {users: MemberListResolver}},
-      {path: 'members/:id', component: MemberDetailComponent, resolve: {user: MemberDetailResolver}},
+      {path: 'members', component: MemberListComponent,
+        resolve: {users: MemberListResolver}},
+      {path: 'members/:id', component: MemberDetailComponent,
+        resolve: {user: MemberDetailResolver}},
+      {path: 'member/edit', component: MemberEditComponent,
+        resolve: {user: MemberEditResolver},
+        canDeactivate: [PreventUnsavedChanges]},
       {path: 'messeges', component: MessegesComponent},
       {path: 'lists', component: ListsComponent}
     ]
